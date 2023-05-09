750e03b9ad9438f0f2c43508decb612248d96605
Aimar <aimaradhitya@gmail.com>
2023-05-09 20:29:21 +0800

    Minor fixes.

--- app/src/main/cpp/Utils/Log.h
+++ app/src/main/cpp/Utils/Log.h
@@ -1,7 +1,7 @@
-#include "../../../../../../../AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/include/c++/v1/stdio.h"
-#include "../../../../../../../AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/include/unistd.h"
+#include <stdio.h>
+#include <unistd.h>
 
-#include "../../../../../../../AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/include/android/log.h"
-#include "../../../../../../../AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/include/c++/v1/string"
+#include <android/log.h>
+#include <string>
 
-#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "NativeGuard", __VA_ARGS__))
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "NativeGuard", __VA_ARGS__)) // Disable this on production
